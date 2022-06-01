a487a465befb2e89135bfbea742a8bf0aabc8bcd
Takuyay <mechanicalgiraffe@gmail.com>
2022-06-01 17:07:41 +0900

    Fix static route

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 const loaders = require("./src/loaders");
 const routes = require("./src/routes");
+const path = require("path");
 
 loaders(app);
 routes(app);
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 module.exports = app;
